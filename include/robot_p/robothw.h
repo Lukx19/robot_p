814b6ea7a80c9bb4ccf74bbe703eb2526214a124
Lukas Jelinek <lukx19@gmail.com>
2017-05-25 13:36:54 +0200

    Implementation of new serial interface based on Readme

--- include/robot_p/robothw.h
+++ include/robot_p/robothw.h
@@ -15,6 +15,25 @@ namespace robotp
 {
 class RobotHW : public hardware_interface::RobotHW
 {
+  struct __attribute__((__packed__)) InMsg {
+    char id;
+    int32_t left;
+    int32_t right;
+    uint8_t crc;
+  };
+
+  struct __attribute__((__packed__)) OutMsg {
+    char id;
+    union {
+      uint32_t param;
+      struct {
+        int16_t left;
+        int16_t right;
+      };
+    };
+    uint8_t crc;
+  };
+
 public:
   /**
    * @brief RobotHW
@@ -41,20 +60,30 @@ public:
 private:
   const size_t LEFT = 0;
   const size_t RIGHT = 1;
+  const static constexpr size_t VELOCITY_SCALE = 255;
   hardware_interface::JointStateInterface jnt_state_interface;
   hardware_interface::VelocityJointInterface jnt_vel_interface;
   serial::Serial serial_;
-  std::array<uint8_t, 4> input_buff_;
-  double max_velocity_;
+  double max_wheel_velocity_;
+  double ticks_per_meter_;
+  std::array<uint8_t, 10> input_buff_;
   double cmd_[2];
   double pos_[2];
   double vel_[2];
   double eff_[2];
 
-  std::tuple<std::array<uint16_t, 2>, bool>
-  parseData(const std::array<uint8_t, 4>& data);
-  std::array<uint8_t, 3> floatTo3Bytes(float val) const;
-  void sendFloat(unsigned char letter, float val);
+  OutMsg createPIDMsg(char letter, float data) const;
+  OutMsg createVelocityMsg(double left_vel, double right_vel) const;
+
+  template <typename T>
+  void sendMsg(const T& msg)
+  {
+    const uint8_t* data = reinterpret_cast<const uint8_t*>(&msg);
+    size_t size = sizeof(T);
+    if (serial_.write(data, size) != size) {
+      ROS_WARN_STREAM("ROBOT_P_CONTROL: sending message was not successful");
+    }
+  }
 };
 }
 
--- src/robothw.cpp
+++ src/robothw.cpp
@@ -6,7 +6,8 @@ RobotHW::RobotHW(ros::NodeHandle &nh_private)
   : jnt_state_interface()
   , jnt_vel_interface()
   , serial_()
-  , max_velocity_(2)
+  , max_wheel_velocity_(2)
+  , ticks_per_meter_(100)
   , input_buff_()
 
 {
@@ -36,112 +37,95 @@ RobotHW::RobotHW(ros::NodeHandle &nh_private)
   std::string port;
   float Kp, Ki, Kd;
 
-  nh_private.param<double>("max_angular_velocity", max_velocity_, 2);
-  max_velocity_ = std::abs(max_velocity_);
+  nh_private.param<double>("ticks_per_meter", ticks_per_meter_, 100);
+  // radians/s
+  nh_private.param<double>("max_wheel_velocity", max_wheel_velocity_, 2);
+  max_wheel_velocity_ = std::abs(max_wheel_velocity_);
+
   nh_private.param<std::string>("serial_port", port, "/dev/ttyS0");
   nh_private.param<float>("Kp", Kp, 1);
   nh_private.param<float>("Ki", Kd, 0.3);
   nh_private.param<float>("Kd", Ki, 0.5);
 
   serial_.setPort(port);
-  serial_.setBaudrate(115200);
+  serial_.setBaudrate(57600);
   try {
     serial_.open();
   } catch (serial::SerialException e) {
     ROS_ERROR_STREAM("[ROBOTHW]: Serial exception: " << e.what());
     return;
   } catch (serial::IOException e) {
-    ROS_ERROR_STREAM("[ROBOTHW]: Serial IO exception: " << e.what());
+    ROS_ERROR_STREAM("ROBOT_P_CONTROL: Serial IO exception: " << e.what());
     return;
   }
 
-  // send PID constants to robot
-  // these messages are in format P/I/D 3bytes of encoded float. To decode this
-  // float divide the value stored in these bytes by 1000. The data bytes are
-  // send in MSB first
-  sendFloat('P', Kp);
-  sendFloat('I', Ki);
-  sendFloat('D', Kd);
+  sendMsg(createPIDMsg('P', Kp));
+  sendMsg(createPIDMsg('I', Ki));
+  sendMsg(createPIDMsg('D', Kd));
 }
 
 void RobotHW::read(const ros::Time &time, const ros::Duration &period)
 {
-  /* reads 4 byte long messages
-   * [0-3600][0-3600]
-   *    -> left and right wheel CCW position represented with 2 bytes holding
-   *       values in range [0-3600] stored MSB first
-   */
-  if (serial_.available() > 4) {
-    serial_.flushInput();
-  }
-  if (serial_.read(input_buff_.data(), 4) != 4) {
-    ROS_ERROR_STREAM("[ROBOTHW]:unable to read 6 bytes from serial input");
-    return;
-  }
-  auto parsed_data = parseData(input_buff_);
-  if (!std::get<1>(parsed_data)) {
-    return;
+  size_t message_count = serial_.available() / 10;
+  std::array<double, 2> ticks = {0, 0};
+  for (size_t i = 0; i < message_count; ++i) {
+    if (serial_.read(input_buff_.data(), 10) != 10) {
+      ROS_ERROR_STREAM(
+          "ROBOT_P_CONTROL:unable to read 10 byte message from serial "
+          "input");
+      return;
+    }
+    InMsg *msg = reinterpret_cast<InMsg *>(input_buff_.data());
+    switch (msg->id) {
+      case 'T':
+        ticks[0] += msg->left;
+        ticks[1] += msg->right;
+        break;
+      default:
+        ROS_ERROR_STREAM(
+            "ROBOT_P_CONTROL: Unknown input message type: " << msg->id);
+        break;
+    }
   }
 
-  double conversion_rate = boost::math::constants::pi<double>() / 1800;
   for (size_t i = 0; i < 2; ++i) {
-    double old_pos = pos_[i];
-    pos_[i] = std::get<0>(parsed_data)[i] * conversion_rate;
-    vel_[i] = (pos_[i] - old_pos) / period.toSec();
+    pos_[i] = 0;
+    vel_[i] = (ticks[i] / ticks_per_meter_) / period.toSec();  // m/s
     eff_[i] = 0;
   }
 }
 
 void RobotHW::write()
 {
-  /* sends 2x4 bytes message
-   * V L P [0-255]
-   *      -> angular_velocity left_wheel CW in range from 0 to 255
-   * V R N [0-255]
-   *      -> angular_velocity right_wheel CCW  in range from 0 to 255
-   */
-  for (size_t i = 0; i < 2; ++i) {
-    unsigned char dir = cmd_[i] < 0 ? 'N' : 'P';
-    unsigned char wheel = i % 2 == 0 ? 'L' : 'R';
-    double trunc_vel =
-        std::abs(cmd_[i]) > max_velocity_ ? max_velocity_ : std::abs(cmd_[i]);
-    uint8_t velocity = static_cast<uint8_t>((trunc_vel / max_velocity_) * 255);
-    std::vector<uint8_t> data = {'V', wheel, dir, velocity};
-    serial_.write(data);
-  }
+  sendMsg(createVelocityMsg(cmd_[0], cmd_[1]));
 }
 
-std::tuple<std::array<uint16_t, 2>, bool>
-RobotHW::parseData(const std::array<uint8_t, 4> &data)
+robotp::RobotHW::OutMsg robotp::RobotHW::createPIDMsg(char letter,
+                                                      float data) const
 {
-  std::array<uint16_t, 2> poses;
-  for (size_t i = 0; i < 2; ++i) {
-    poses[i] = (static_cast<uint16_t>(data[i * 2]) << 8) &
-               static_cast<uint16_t>(data[i * 2 + 1]);
-    if (poses[i] > 3600) {
-      std::string wheel = i % 2 == 0 ? "Left" : "Right";
-      ROS_ERROR_STREAM("[ROBOTHW]: " << wheel
-                                     << "wheel pose is out of bounds [0:3600]"
-                                     << poses[i]);
-      return std::make_tuple(poses, false);
-    }
-  }
-  return std::make_tuple(poses, true);
+  OutMsg msg;
+  msg.id = letter;
+  msg.param = static_cast<uint32_t>(std::trunc(data * 100000));
+  msg.crc = 0;
+  return msg;
 }
 
-std::array<uint8_t, 3> robotp::RobotHW::floatTo3Bytes(float val) const
+robotp::RobotHW::OutMsg
+robotp::RobotHW::createVelocityMsg(double left_vel, double right_vel) const
 {
-  std::array<uint8_t, 3> data;
-  uint32_t val_2b = static_cast<uint32_t>(std::trunc(val * 1000));
-  data[0] = static_cast<uint8_t>((val_2b & 0x00FF0000) >> 16);
-  data[1] = static_cast<uint8_t>((val_2b & 0x0000FF00) >> 8);
-  data[2] = static_cast<uint8_t>(val_2b & 0x000000FF);
-  return data;
-}
+  OutMsg msg;
+  msg.id = 'V';
+  msg.crc = 0;
+  auto calcVelocity = [this](double vel) -> int16_t {
+    double trunc_vel = std::abs(vel) > max_wheel_velocity_ ?
+                           max_wheel_velocity_ :
+                           std::abs(vel);
+    int16_t velocity = static_cast<int16_t>((trunc_vel / max_wheel_velocity_) *
+                                            VELOCITY_SCALE);
+    return vel < 0 ? velocity * -1 : velocity;
+  };
 
-void robotp::RobotHW::sendFloat(unsigned char letter, float val)
-{
-  auto data = floatTo3Bytes(val);
-  std::vector<uint8_t> data_v = {letter, data[0], data[1], data[2]};
-  serial_.write(data_v);
+  msg.left = calcVelocity(left_vel);
+  msg.right = calcVelocity(right_vel);
+  return msg;
 }
